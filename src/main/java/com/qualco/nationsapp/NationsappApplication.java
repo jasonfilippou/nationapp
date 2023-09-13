99679abece7caf330b52120e013770285d664d77
Jason Filippou <jason.filippou@gmail.com>
2023-09-13 15:06:14 +0300

    Trying to load SQL files.

---
+++ src/main/java/com/qualco/nationsapp/NationsappApplication.java
@@ -0,0 +1,13 @@
+package com.qualco.nationsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationsappApplication.class, args);
+	}
+
+}
